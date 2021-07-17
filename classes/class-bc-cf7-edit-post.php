4400126d666415ccdceb12f361676a7400c79662
beavercoffee <beavercoffee@vidsoe.com>
2021-07-17 17:55:33 -0600

    BC

--- classes/class-bc-cf7-edit-post.php
+++ classes/class-bc-cf7-edit-post.php
@@ -188,7 +188,11 @@ if(!class_exists('BC_CF7_Edit_Post')){
 			if('contact-form-7' !== $tag){
                 return $output;
             }
-            if(!$this->is_type()){
+            $contact_form = wpcf7_get_current_contact_form();
+            if(null === $contact_form){
+                return $output;
+            }
+            if(!$this->is_type($contact_form)){
                 return $output;
             }
             $post_id = $this->get_post_id();
